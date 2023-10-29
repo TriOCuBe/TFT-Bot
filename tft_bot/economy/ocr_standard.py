56d3b4e8f989464dee2b87c87a3043670feb7c33
TriOCuBe <crimeblocky73@gmail.com>
2023-10-30 00:39:11 +0100

    .

--- tft_bot/economy/ocr_standard.py
+++ tft_bot/economy/ocr_standard.py
@@ -51,33 +51,33 @@ class OCRStandardEconomyMode(EconomyMode):
         if minimum_round <= 3:
             return
 
-        # if minimum_round == 1 or (gold >= 5 and minimum_round == 2):
-        #     self.purchase_units(amount=3)
-        #     time.sleep(0.5)
-        #     return
-        # elif gold >= 35 and minimum_round <= 3:
-        #     self.purchase_units(amount=3)
-        #     if random.randint(0, 3) == 1:
-        #         self.sell_units(amount=2)
-        #     return
-        # elif gold >= 55:
-        #     self.purchase_units(amount=3)
+        if minimum_round == 1 or (gold >= 5 and minimum_round == 2):
+            self.purchase_units(amount=3)
+            time.sleep(0.5)
+            return
+        elif gold >= 35 and minimum_round <= 3:
+            self.purchase_units(amount=3)
+            if random.randint(0, 3) == 1:
+                self.sell_units(amount=2)
+            return
+        elif gold >= 55:
+            self.purchase_units(amount=3)
 
-        # if event:
-        #     event = False
-        #     logger.debug("Triggering event, selling a bunch of champs")
+        if event:
+            event = False
+            logger.debug("Triggering event, selling a bunch of champs")
 
-        #     self.sell_units(amount=5)
-        #     sleep(0.5)
+            self.sell_units(amount=5)
+            sleep(0.5)
 
-        #     self.roll()
-        #     sleep(0.5)
-        #     self.purchase_units(3)
-        #     sleep(0.5)
+            self.roll()
+            sleep(0.5)
+            self.purchase_units(3)
+            sleep(0.5)
             
-        #     for _ in range(3):
-        #         self.walk_random()
-        #         sleep(1.5)
+            for _ in range(3):
+                self.walk_random()
+                sleep(1.5)
 
         gold = screen_helpers.get_gold_with_ocr()
         logger.debug(f"OCR recognized {gold} gold buying and selling")
